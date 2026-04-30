4c76952ef104a426932a1b8c43a6133aca0ba1a2
Phongsavanh Mongkhonvilay <a_mac@katsumoto1226.local>
2026-04-30 13:13:30 -0500

    style change

--- dotnet-server/obj/Debug/net10.0/dotnet-server.AssemblyInfo.cs
+++ dotnet-server/obj/Debug/net10.0/dotnet-server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7078befc23fc661f419000794c46e531e1374b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fae0eaf65177743996033fb975caa5e3c36d6789")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
